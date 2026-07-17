dc9bce7836bf028838c1a38ec9c80d3e2467c674
Vaishnavi Tiwari <vaishnavitiwari603@gmail.com>
2026-07-17 14:20:53 +0530

    created ex1.js file

---
+++ ex1.js
@@ -0,0 +1,14 @@
+//let a=10;
+//const b=25;
+//console.log("a=",a);
+//console.log("b=",b);
+ //b=a++;
+//console.log("b=",b);
+
+//function sum(a,b){
+//    return a+b
+//}
+const sum=(a,b)=>a+b;
+console.log("sum=",sum(10,20));
+console.log("sum=",sum(100,20));
+console.log("sum=",sum(40,20));
